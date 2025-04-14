eb1b233bea0b087e5fae5748511396de025310f4
Huiping Lin <45916876+phoenixrain-pku@users.noreply.github.com>
2025-04-14 22:51:11 +0800

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -17,7 +17,7 @@ if (!window.requestAnimationFrame) {
 
 
 function jump() {
-    DeltaTime = 0.6;
+    DeltaTime = 0.9;
     Player.y = Player.y - Player.Yacceleration * DeltaTime;
     Player.Yacceleration -= 0.5 * DeltaTime;
     if (Player.Yacceleration < 0) {
